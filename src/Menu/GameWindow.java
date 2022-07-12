2c69b615473ca21fd923b09a8f7e1553be888b7d
Carlos Chiva <Aluchi1821@ieselcaminas.org>
2022-07-12 11:37:04 +0200

    next Step: change the dimension of table and edit location of Panel and add sources and game over

--- src/Menu/GameWindow.java
+++ src/Menu/GameWindow.java
@@ -3,30 +3,21 @@ package Menu;
 import Table.TableManager;
 
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
-import javax.swing.*;
 
-import static Enum.Enum.CURRENTPIECE;
 import static Enum.Enum.EMPTY;
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 
 public class GameWindow extends JPanel {
-    JFrame jFrame = new JFrame();
+
     TableManager tableManager;
     int cuadrado = 25;
     public GameWindow() {
-
         tableManager = new TableManager();
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(250, 200));
-        jFrame.setResizable(false);
-        jFrame.add(this);
-        jFrame.pack();
-        jFrame.setLocationRelativeTo(null);
         setVisible(true);
-        jFrame.add(this);
+
     }
 
     protected void paintComponent(Graphics g) {
@@ -46,36 +37,6 @@ public class GameWindow extends JPanel {
                 }
             }
 
-/*
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 12; j++) {
-                if (tableManager.table.valueInTableOf(i, j) == CURRENTPIECE) {
-                    g.fillRect(i * cuadrado, j * cuadrado, this.getWidth() * cuadrado, this.getHeight() * cuadrado);
-                }
-            }
-        }
-*/
     }
 
-    /*
-    public void refresh() {
-        int x = 0;
-        for (int i = 0; i < heightPanel; i += heightPanel) {
-            int y = 0;
-            for (int j = 0; j < withPanel; j += withPanel) {
-                if (tableManager.table.valueInTableOf(x, y) == EMPTY) {
-
-                }
-
-            }
-        }
-    }
-
-    public void paint(Graphics g, int x, int y) {
-        super.paint(g);
-        g.setColor(Color.CYAN);
-        g.fillRect(x + 1, y + 1, anchoCuadrado, altoCuadrado);
-    }
-
-*/
 }
--- src/Menu/Window.java
+++ src/Menu/Window.java
@@ -2,17 +2,39 @@ package Menu;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Window extends JFrame {
-    JPanel jPanel;
+
+public class Window extends JFrame implements KeyListener {
+    GameWindow jPanel;
 
     public Window() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
         pack();
+        addKeyListener(this);
         setLocationRelativeTo(null);
         setVisible(true);
-        jPanel=new GameWindow();
+        setIgnoreRepaint(true);
+        jPanel = new GameWindow();
         add(jPanel);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+      jPanel.tableManager.movedPoint(e.getKeyChar());
+      jPanel.validate();
+      jPanel.repaint();
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
